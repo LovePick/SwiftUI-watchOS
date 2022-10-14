92e5f0ca1953c9616c9d650bc5b3fb95c8fd9832
pick <picknavin556@gmail.com>
2022-10-14 20:31:42 +0700

    initi

---
+++ Notes/Notes Watch App/Screen/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Supapon Pucknavin on 14/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
